30a9e7a4cff6382810d549d3255f6fe48b99f61f
Alexander Papageorgiou <oriong85@gmail.com>
2013-07-10 04:07:04 +0300

    Add description for the ADDIU instruction.

--- Allegrex/Basic/ADDIU.h
+++ Allegrex/Basic/ADDIU.h
@@ -4,6 +4,16 @@
 
 #include "../Instruction.h"
 
+/************************************************************************ p.A-12
+ * ADDIU rt, rs, immediate
+ *
+ * The 16-bit immediate is sign-extended and added to the contents of general
+ * register rs to form the result. The result is placed into general register rt.
+ * No integer overflow exception occurs under any circumstances.
+ *
+ * The only difference between this instruction and the ADDI instruction is that
+ * ADDIU never causes an overflow exception.
+ */
 class ADDIU :
 	public Instruction
 {
