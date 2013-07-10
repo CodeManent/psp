6c736cab444227eb8931768b5ad1f58f4dbe87d5
Alexander Papageorgiou <oriong85@gmail.com>
2013-07-10 04:04:17 +0300

    Add description for the ADDI instruction.

--- Allegrex/Basic/ADDI.h
+++ Allegrex/Basic/ADDI.h
@@ -4,6 +4,16 @@
 
 #include "../Instruction.h"
 
+/************************************************************************ p.A-12
+ * ADDI rt, rs, immediate
+ *
+ * The 16-bit immediate is sign-extended and added to the contents of general
+ * register rs to form the result. The result is placed into general register rt.
+ *
+ * An overflow exception occurs if the carries out of bits 30 nd 31 differ
+ * (2s complement overflow). The destinatino register rd is not modified when
+ * an integer overflow exception occurs.
+ */
 class ADDI :
 	public Instruction
 {
