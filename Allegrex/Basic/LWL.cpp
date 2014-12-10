3cfa7596419b23dd15b108bd70258b5ab09adca4
Αλέξανδρος Παπαγεωργίου <oriong85@gmail.com>
2014-12-10 23:58:07 +0200

    Revisit and fix LWL

--- Allegrex/Basic/LWL.cpp
+++ Allegrex/Basic/LWL.cpp
@@ -13,19 +13,23 @@ LWL::~LWL(void)
 std::string LWL::disassemble() const{
 	std::stringstream ss;
 
-	ss << "lwl\t" << strRt() << ", " << strRs() << "(" << strSignedImmediate()  << ")";
+	ss << "lwl\t" << strRt() << ", " << strSignedImmediate() << '(' << strRs() << ')';
 
 	return ss.str();
 }
 
 void LWL::execute(Allegrex &cpu) const{
-	// TODO: check correctness
 	const uint32 vAddr = signExtendImm() + cpu.GPR[u.i.rs];
 	const uint32 mask = ~(0x03);
 	auto byte = vAddr & ~mask;
 
 	uint32 mem = cpu.systemCoprocessor.loadMemory32(vAddr & mask);
 
-	mem <<= byte;
+	const uint32 shift = (3 - byte) * 8;
+	const uint32 writemask = (0xFFFFFFFF << shift);
+
+	mem <<= shift;
+	cpu.GPR[u.i.rt] &= ~writemask;
 	cpu.GPR[u.i.rt] |= mem;
 }
+
